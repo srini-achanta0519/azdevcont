8cc4b1925704e82a11bd995b3d950899ed00c5bb
srini-achanta0519 <152132575+srini-achanta0519@users.noreply.github.com>
2025-04-27 22:26:25 +0000

    Update Terraform configuration: Add resource group module and update configuration files

---
+++ TerraformIaC/resource_group.tf
@@ -0,0 +1,32 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region where resources will be created"
+  type        = string
+  default     = "eastus"
+}
+
+variable "tags" {
+  description = "Map of tags to apply to the resource group"
+  type        = map(string)
+  default     = {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+output "resource_group_id" {
+  description = "ID of the created resource group"
+  value       = azurerm_resource_group.rg.id
+}
+
+output "resource_group_name" {
+  description = "Name of the created resource group"
+  value       = azurerm_resource_group.rg.name
+}
--- TerraformIaC/terraform.tf
+++ TerraformIaC/terraform.tf
@@ -1,37 +1,14 @@
 terraform {
-  required_version = ">=1.3.0"
-
-  cloud {
-    organization = "codespaces"
-    workspaces {
-      name = "Codespaces"
-    }
-  }
+  required_version = ">= 1.3.0"
 
   required_providers {
-
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.30.0"
+      version = ">= 3.0.0"
     }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~>4.0"
-    }
-
   }
 }
 
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
+  features {}
 }
