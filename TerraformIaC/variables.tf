d0e14a216b0b561a2847b9d78389685445dec1d9
srini-achanta0519 <152132575+srini-achanta0519@users.noreply.github.com>
2025-08-31 00:03:17 +0000

    Added Azure Variables

---
+++ TerraformIaC/variables.tf
@@ -0,0 +1,16 @@
+variable "ARM_TENANT_ID" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+variable "ARM_SUBSCRIPTION_ID" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+variable "ARM_CLIENT_ID" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+variable "ARM_CLIENT_SECRET" {
+  description = "Azure Tenant ID"
+  type        = string
+}
